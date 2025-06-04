c0c83eb1f68801a97b6fba4352b826141b417079
Andrew Luo <jihanjie.luo@gmail.com>
2025-06-04 16:18:01 -0400

    Code cleanup for sign up

--- app/src/main/java/com/example/cs446/data/model/Pet.kt
+++ app/src/main/java/com/example/cs446/data/model/Pet.kt
@@ -1,15 +1,15 @@
 package com.example.cs446.data.model
 import com.squareup.moshi.Json
-import java.time.OffsetDateTime
-import java.time.LocalDate
+import kotlinx.datetime.Instant
+import java.util.UUID
 
 data class Pet(
     val id: Int,
-    @Json(name = "created_at") val createdAt: String,
+    @Json(name = "created_at") val createdAt: Instant,
     val name: String,
     val species: Int,
     val breed: String?,
-    @Json(name = "creator_id") val creatorId: Int,
-    val birthdate: String,
+    @Json(name = "creator_id") val creatorId: UUID,
+    val birthdate: Instant,
     val weight: Double
 )
--- app/src/main/java/com/example/cs446/data/model/User.kt
+++ app/src/main/java/com/example/cs446/data/model/User.kt
@@ -1,5 +1,6 @@
 package com.example.cs446.data.model
 import com.squareup.moshi.Json
+import kotlinx.datetime.Instant
 import java.time.OffsetDateTime
 import java.time.LocalDate
 import java.util.UUID
@@ -7,7 +8,7 @@ import kotlin.uuid.Uuid
 
 data class User(
     val id: UUID,
-    @Json(name = "created_at") val createdAt: String,
+    @Json(name = "created_at") val createdAt: Instant,
     val username: String,
     val email: String,
     @Json(name = "avatar_url") val avatarUrl: String? = null
